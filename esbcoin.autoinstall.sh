568a757d1e2ceb00594696af208b79e0a033e708
BlockchainFor <42303494+BlockchainFor@users.noreply.github.com>
2019-01-26 14:33:03 +0300

    Update esbcoin.autoinstall.sh

--- esbcoin.autoinstall.sh
+++ esbcoin.autoinstall.sh
@@ -108,22 +108,8 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
-sudo apt-get -y install wget nano htop jq
-sudo apt-get -y install libzmq3-dev
-sudo apt-get -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
-sudo apt-get -y install libevent-dev -y
-sudo apt-get instal unzip
-sudo apt -y install software-properties-common
-sudo add-apt-repository ppa:bitcoin/bitcoin -y
-sudo apt-get -y update
-sudo apt-get -y install libdb4.8-dev libdb4.8++-dev -y
-sudo apt-get install unzip
-sudo apt-get -y install libminiupnpc-dev
-sudo apt-get -y install fail2ban
+sudo apt-get -y install wget nano htop jq unzip fail2ban
 sudo service fail2ban restart
-sudo apt-get install libdb5.3++-dev libdb++-dev libdb5.3-dev libdb-dev && ldconfig -y
-sudo apt-get install -y unzip libzmq3-dev build-essential libssl-dev libboost-all-dev libqrencode-dev libminiupnpc-dev libboost-system1.58.0 libboost1.58-all-dev libdb4.8++ libdb4.8 libdb4.8-dev libdb4.8++-dev libevent-pthreads-2.0-5 -y
-sudo apt-get install libdb5.3++-dev libdb++-dev libdb5.3-dev libdb-dev && ldconfig -y
    fi
 
 #Network Settings
